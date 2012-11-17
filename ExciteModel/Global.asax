e52ca34b62da01ee773f1f16c621028e1b2b19bb
Adrian Vladu <adyv_adyv@yahoo.com>
2012-11-17 21:56:24 +0200

    Changed the db models

---
+++ ExciteModel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExciteModel.MvcApplication" Language="C#" %>
---
+++ ExciteModels/ExciteContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Globalization;
+using System.Web.Security;
+
+namespace Excite.Models
+{
+    public class ExciteContext : DbContext
+    {
+        public ExciteContext()
+            : base("ExciteConnection")
+        {
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+
+  
+    
+}
